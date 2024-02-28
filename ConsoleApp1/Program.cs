dc2b6504dd2cc1264c6f8cda132e1d41c1c43131
“FeryazHacimuradov” <tu201906125@code.edu.az>
2024-02-28 12:01:35 +0400

    Valid Parentheses

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,62 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+class Program
+{
+    static public string LongestCommonPrefix(string[] strs)
+    {
+        string result = "";
+
+        if (strs == null || strs.Length == 0)
+            return "";
+
+        string pref = strs[0];
+
+        for (int i = 0; i < pref.Length; i++)
+        {
+            for (int j = 1; j < strs.Length; j++)
+            {
+                if (i >= strs[j].Length || pref[i] != strs[j][i])
+                {
+                    return result;
+                }
+            }
+            result += pref[i];
+        }
+        return result;
+    }
+
+    static public bool IsValid(string s) {
+        Stack<char> stack = new Stack<char>();
+        
+        foreach (char c in s) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.Push(c);
+            } else {
+                if (stack.Count == 0) {
+                    return false; 
+                }
+                
+                char top = stack.Pop();
+                if ((c == ')' && top != '(') || 
+                    (c == '}' && top != '{') || 
+                    (c == ']' && top != '[')) {
+                    return false; 
+                }
+            }
+        }
+        
+        return stack.Count == 0; 
+    }
+
+    static void Main(string[] args)
+    {
+        // string[] strs = { "flower", "flow", "flight" };
+        // string[] strs1 = { "dog","racecar","car" };
+
+        // Console.WriteLine(LongestCommonPrefix(strs1));
+        
+        Console.WriteLine(IsValid("()")); // true
+        // Console.WriteLine(IsValid("()[]{}")); // true
+        // Console.WriteLine(IsValid("(]"));
+    } 
+}
