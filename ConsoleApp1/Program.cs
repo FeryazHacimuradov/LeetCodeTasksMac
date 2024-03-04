ba1654bc52152336eaafa03b6a5aa919070dc987
“FeryazHacimuradov” <tu201906125@code.edu.az>
2024-03-04 12:03:04 +0400

    Search Insert Position task

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -47,16 +47,37 @@ class Program
         
         return stack.Count == 0; 
     }
+    
+    static public int SearchInsert(int[] nums, int target)
+    {
+        int output = 0;
 
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] == target)
+            {
+                output = i;
+                break;
+            }
+            if (nums[i] < target)
+            {
+                output = i + 1;
+            }
+        }
+
+        return output;
+    }
+        
     static void Main(string[] args)
     {
+        Console.WriteLine(SearchInsert([1,3,5,6], 5));
         // string[] strs = { "flower", "flow", "flight" };
         // string[] strs1 = { "dog","racecar","car" };
 
         // Console.WriteLine(LongestCommonPrefix(strs1));
         
-        Console.WriteLine(IsValid("()")); // true
-        // Console.WriteLine(IsValid("()[]{}")); // true
+        // Console.WriteLine(IsValid("()"));
+        // Console.WriteLine(IsValid("()[]{}"));
         // Console.WriteLine(IsValid("(]"));
     } 
 }
